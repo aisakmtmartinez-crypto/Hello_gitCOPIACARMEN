4ec153e102d251ed481a4a55ed256cf3a42d02c4
Isaac <aisakmtmartinez@gmail.com>
2025-11-15 22:38:06 -0600

    Error solucionado despues de cambiarme de rama y guardarlo en stash temporalmente

--- inventario.py
+++ inventario.py
@@ -2,3 +2,5 @@ import pandas as pd
 data = [["Alan", 18], ["Josue",20]]
 df = pd.DataFrame(data, columns = ["Nombre", "Edad"])
 print(df)
+
+print("No se como hacerlo por el moennto pero con stash lo dejare para despies")
