844defcdab41cd982e0c9a54946fbaa19e8693a6
Isaac <aisakmtmartinez@gmail.com>
2025-12-03 19:33:34 -0600

    Nuevo probando py, para causar conflicto en el repo de Carmen y que lo solucione

--- probando.py
+++
@@ -1,3 +0,0 @@
-print("A lo mejor funciona")
-
-print("puedo editar la copia del repo de carmen pero en mi loca, veremos para hacer pull request despues")
---
+++ un_nuevo_probando.py
@@ -0,0 +1,2 @@
+print("Borre el anterior probando y agregue un nuevo para causar un conflicto con el repo original de carmen")
+print("Necesitamos arreglar el conflicto")
